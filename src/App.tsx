21b196f225bf0040eb99a2711ad8757e1cd6a2fa
Stephen Roof <steveroof@gmail.com>
2024-06-17 17:08:26 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,31 @@ import { myData } from "./data";
 export const App: React.FC = () => {
   const { summary, tenures, education, skills } = myData;
 
+  const jobsMarkup = tenures.map((t, index) => (
+    <Tenure key={index} {...t}>
+      {t.jobs.map((p, index) => (
+        <Position key={index} {...p} />
+      ))}
+    </Tenure>
+  ));
+
+  const educationMarkup = education.map((e, index) => (
+    <Education key={index} {...e} />
+  ));
+
+  const skillsMarkup = skills.map((s, index) => <li key={index}>{s}</li>);
+
   return (
     <>
       <h1>Stephen Roof</h1>
       <h2>About</h2>
       <div>{summary}</div>
       <h2>Experience</h2>
-      {tenures.map((t, index) => (
-        <Tenure key={index} {...t}>
-          {t.jobs.map((p, index) => (
-            <Position key={index} {...p} />
-          ))}
-        </Tenure>
-      ))}
+      {jobsMarkup}
       <h2>Education</h2>
-      {education.map((e, index) => (
-        <Education key={index} {...e} />
-      ))}
+      {educationMarkup}
       <h2>Skills</h2>
-      <ul>
-        {skills.map((s, index) => (
-          <li key={index}>{s}</li>
-        ))}
-      </ul>
+      <ul>{skillsMarkup}</ul>
     </>
   );
 };
